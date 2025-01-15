0bd7f659466d7136b619e079784740fe78b650dd
221B298 <221B298@juet.co.in>
2025-01-15 15:34:39 +0530

    getset folder

---
+++ ap0/juet.java
@@ -0,0 +1,32 @@
+
+
+import java.io.*;
+
+class GetSet {
+
+
+	private String name;
+
+	public String getName() { return name; }
+
+	public void setName(String N)
+	{
+
+		this.name = N;
+	}
+}
+
+class juet {
+
+	public static void main(String[] args)
+	{
+	
+		GetSet obj = new GetSet();
+
+		
+		obj.setName("jaypee university");
+		
+		System.out.println(obj.getName());
+	}
+}
+   
